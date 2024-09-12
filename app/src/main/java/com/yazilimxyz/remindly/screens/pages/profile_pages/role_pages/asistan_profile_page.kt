d1a5a27499e5dfb06adfad707feea6f528bd2b34
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2024-09-12 13:57:25 +0300

    will try modifying getRoleCredentials

---
+++ app/src/main/java/com/yazilimxyz/remindly/screens/pages/profile_pages/role_pages/asistan_profile_page.kt
@@ -0,0 +1,2 @@
+package com.yazilimxyz.remindly.screens.pages.profile_pages.role_pages
+
---
+++ app/src/main/java/com/yazilimxyz/remindly/screens/pages/profile_pages/role_pages/calisan_profile_page.kt
@@ -0,0 +1,4 @@
+package com.yazilimxyz.remindly.screens.pages.profile_pages.role_pages
+
+class calisan_profile_page {
+}
